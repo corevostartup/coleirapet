1892d80fe580d53a5464f2a9197f6575c66b0276
Cassio <cassio@example.com>
2026-04-04 00:33:29 -0300

    Add missing iOS project files and ignore rules

---
+++ ColeiraPet/ColeiraPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColeiraPetApp.swift
+//  ColeiraPet
+//
+//  Created by Cássio on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ColeiraPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColeiraPetTests/ColeiraPetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColeiraPetTests.swift
+//  ColeiraPetTests
+//
+//  Created by Cássio on 03/04/26.
+//
+
+import Testing
+@testable import ColeiraPet
+
+struct ColeiraPetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
